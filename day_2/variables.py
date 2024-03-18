60d1a209a5e397cc9491592cc46fab2a17af1bf2
ahtape <athasi.kurus@gmail.com>
2024-03-18 18:56:55 +0700

    day 2

---
+++ day_2/variables.py
@@ -0,0 +1,46 @@
+#Day 2: 30 Days of Python programming
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+full_name = first_name + " " + last_name
+country = "Indonesia"
+city = "Yogyakarta"
+age = 22
+year = 2024
+is_married = False
+is_true = True
+is_light_on = True
+university, major = "Universitas Gadjah Mada", "Accounting"
+
+type_first_name = type(first_name)
+type_last_name = type(last_name)    
+type_full_name = type(full_name)
+type_country = type(country)
+type_city = type(city)
+type_age = type(age)
+type_year = type(year)
+type_is_married = type(is_married)
+type_is_true = type(is_true)
+type_is_light_on = type(is_light_on)
+type_university = type(university)
+type_major = type(major)
+
+length_first_name = len(first_name)
+length_last_name = len(last_name)
+
+num_one = 5
+num_two = 4
+
+total = num_one + num_two
+diff = num_one - num_two
+product = num_one * num_two
+division = num_one / num_two
+remainder = num_one % num_two
+exp = num_one ** num_two
+floor_division = num_one // num_two
+
+circle_radius = int(input("Enter the radius of the circle: "))
+circle_area = 3.14 * circle_radius ** 2
+circle_circumference = 2 * 3.14 * circle_radius
+
+help("keywords")
