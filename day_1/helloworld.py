af84b2d49b65e9a6c2f7f2957a0383119b3df59e
ahtape <athasi.kurus@gmail.com>
2024-03-18 12:31:06 +0700

    initial commit

---
+++ day_1/helloworld.py
@@ -0,0 +1,66 @@
+# Exercise level 2
+print(3 + 4)
+print(3 - 4)
+print(3 * 4)
+print(3 % 4)
+print(3 / 4)
+print(3 ** 4)
+print(3 //4)
+
+name = "Atha"
+print(name)
+
+family_name = "Fausta"
+print(family_name)
+
+country = "Indonesia"
+print(country)
+
+enjoy = "I am enjoying 30 days of python"
+print(enjoy)
+
+print(type(10))
+print(type(0.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(["Asabeneh", "Python", "Finland"]))
+print(type(name))
+print(type(family_name))
+print(type(country))
+
+
+#Exercise level 3
+xinteger = 3
+print(xinteger)
+
+xfloat = 3.1
+print(xfloat)
+
+xcomplex = 2 + 3j
+print(xcomplex)
+
+xboolean = True
+
+xlist = ["apple", "banana", "cherry"]
+print(xlist)
+
+xtuple = ("apple", "banana", "cherry")
+print(xtuple)
+
+xset = {"apple", "banana", "cherry"}
+print(xset)
+
+xdict = {
+    "name": "Atha",
+    "age": 22
+}
+print(xdict)
+
+#euclidean distance
+x1 = 2
+y1 = 3
+x2 = 10
+y2 = 8
+
+distance = ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+print(distance)
